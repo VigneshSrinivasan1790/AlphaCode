89f6cac794dd33731c571a90904dc0a86d6ef47d
Sunita Kapoor <Sunita.Kapoor@coxautoinc.com>
2017-01-10 23:29:53 -0500

    initial commit

---
+++ source/AlphaCodeSource/WebContent/script.js
@@ -0,0 +1,7 @@
+
+function Register(){
+    if(confirm('Register?')){
+        var register = document.getElementById('register').value;
+        data.innerHTML = '<div>Registeration Completed!.</div>';
+    }
+}
