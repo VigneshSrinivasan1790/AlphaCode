c019a245903f7a88d6111a8164f3b14c71f0f949
senthiltau <senthilkumar.krishnan90@gmail.com>
2017-01-17 09:18:04 +0530

    Added sql script for creating user registration table

---
+++ source/AlphaCodeSource/sql scripts/userRegistration_Create_table.sql
@@ -0,0 +1,14 @@
+USE TEAMALPHA;
+
+
+DROP TABLE IF EXISTS userRegistration;
+
+CREATE TABLE userRegistration (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	userName VARCHAR(25) NOT NULL,
+	emailId VARCHAR(50) NOT NULL,
+    phoneNumber VARCHAR(20) NOT NULL,
+	userPassword VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE(emailId)
+  );
